e902b105986b22806e744944d256083c500934b2
idk11 <41804965+idk11@users.noreply.github.com>
2018-07-28 14:49:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ KiNg66S.on('message', message => {
 
   var argresult = args.join(' ');
 
-  if (message.author.id !== "350968179927023619") return;
+  if (message.author.id !== "441704732344582146") return;
 
   
 
